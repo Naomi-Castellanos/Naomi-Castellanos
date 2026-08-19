02005632f85e7edff9e385ff55bb2e4bc2d9a3ff
Naomi Castellanos <ncastellanosjz@gmail.com>
2026-08-19 17:11:03 -0400

    Everything finished except Art section

--- Tech-Stack/Interactive.js
+++ Tech-Stack/Interactive.js
@@ -645,295 +645,55 @@ window.addEventListener(
     }
 );
 
+const title = document.querySelector(".hover-h1");
+const normalTitle = title.querySelector(".normal-title");
+const translations = title.querySelectorAll(".hover-text-content");
 
+let currentIndex = 0;
+let interval;
 
-// console.log("JavaScript is working!");
+title.addEventListener("mouseenter", function () {
 
-// const cards = document.querySelectorAll(".project-card");
+    // Hide the original name
+    normalTitle.style.display = "none";
 
-// const category = document.getElementById("project-category");
-// const title = document.getElementById("project-title");
-// const description = document.getElementById("project-description");
-// const creators = document.getElementById("project-creators");
+    // Show the first translation
+    translations[currentIndex].style.display = "inline";
 
+    // Keep changing the language
+    interval = setInterval(function () {
 
-// // Function that changes the text
-// function changeProjectText(card) {
+        // Hide current translation
+        translations[currentIndex].style.display = "none";
 
-//     const newCategory = card.dataset.category;
-//     const newTitle = card.dataset.title;
-//     const newDescription = card.dataset.description;
-//     const newCreators = card.dataset.creators;
+        // Move to the next translation
+        currentIndex++;
 
+        // Go back to the beginning
+        if (currentIndex >= translations.length) {
+            currentIndex = 0;
+        }
 
-//     // Fade out
-//     category.style.opacity = "0";
-//     title.style.opacity = "0";
-//     description.style.opacity = "0";
-//     creators.style.opacity = "0";
+        // Show the next translation
+        translations[currentIndex].style.display = "inline";
 
+    }, 700); // changes every 1 second
+});
 
-//     setTimeout(() => {
 
-//         category.textContent = newCategory;
+title.addEventListener("mouseleave", function () {
 
-//         title.textContent = newTitle;
+    // Stop changing languages
+    clearInterval(interval);
 
-//         description.textContent = newDescription;
+    // Hide all translations
+    translations.forEach(function(translation) {
+        translation.style.display = "none";
+    });
 
-//         creators.innerHTML =
-//             newCreators.replace("|", "<br>");
+    // Show original name
+    normalTitle.style.display = "inline";
 
-
-//         // Fade in
-//         category.style.opacity = "1";
-//         title.style.opacity = "1";
-//         description.style.opacity = "1";
-//         creators.style.opacity = "1";
-
-//     }, 150);
-// }
-
-
-
-// // ======================================
-// // SCROLL DETECTION
-// // ======================================
-
-// const observer = new IntersectionObserver(
-
-//     (entries) => {
-
-//         entries.forEach(entry => {
-
-//             if (entry.isIntersecting) {
-
-//                 // Change the text to match
-//                 // the picture currently visible
-//                 changeProjectText(entry.target);
-
-//             }
-
-//         });
-
-//     },
-
-//     {
-//         threshold: 0.6
-//     }
-
-// );
-
-
-// // Watch every project card
-// cards.forEach(card => {
-
-//     observer.observe(card);
-
-// });
-
-
-
-
-
-// const card = document.querySelector(".project-card");
-
-// const back = document.querySelector(".back");
-// const middle = document.querySelector(".middle");
-// const front = document.querySelector(".front");
-// const image = document.querySelector(".image-layer");
-
-// const projectTitle = document.getElementById("project-title");
-// const projectName = document.getElementById("project-name");
-// const projectDescription = document.getElementById("project-description");    
-// const projectMembers = document.getElementById("project-members");
-
-
-
-
-// const projects = [
-
-//     {
-//         title: "COLLABORATIVE",
-
-//         name: "MYTASK",
-
-//         description:
-//             "A digital space that facilitate a deeper concentration for those who daily procrastinate on their daily tasks.",
-
-//         members: [
-//             "London", 
-//             "Naomi Castellanos - AFH",
-//         ]
-//     },
-
-
-//     {
-//         title: "COLLABORATIVE",
-
-//         name: "Gaia",
-
-//         description:
-//             "A Manta ray-robot, named Gaia, which purpose is to save and prevent ocean pollution by automaticly collecting trash in the water. So then the trash is converted into a eco-brick to build houses for low-income families.",
-
-//         members: [
-//             "Biobots - Colegio Bilingüe El Castaño Robotic team",
-//         ]
-//     },
-
-
-//     {
-//         title: "Coming",
-
-//         name: "---",
-
-//         description:
-//             "----.",
-
-//         members: [
-//             "---"
-//         ]
-//     },
-
-
-//     {
-//         title: "Coming",
-
-//         name: "---",
-
-//         description:
-//             "-----",
-
-//         members: [
-//             "-----"
-//         ]
-//     }
-
-// ];
-
-// function changeProject(index) {
-
-//     projectTitle.textContent =
-//         projects[index].title;
-
-//     projectName.textContent =
-//         projects[index].name;
-
-//     projectDescription.textContent =
-//         projects[index].description;
-//     projectMembers.innerHTML =
-//         projects[index].members
-//             .map(member => `<span>${member}</span>`)
-//             .join("<br>");
-
-// }
-
-// card.addEventListener("mousemove", function(event) {
-
-//     const rect = card.getBoundingClientRect();
-
-
-//     const mouseX =
-//         (event.clientX - rect.left)
-//         / rect.width
-//         * 2 - 1;
-
-
-//     const mouseY =
-//         (event.clientY - rect.top)
-//         / rect.height
-//         * 2 - 1;
-
-
-//     /* BACK */
-
-//     back.style.transform =
-//         `translate(
-//             ${mouseX * 8}px,
-//             ${mouseY * 8}px
-//         )`;
-
-
-//     /* MIDDLE */
-
-//     middle.style.transform =
-//         `translate(
-//             ${mouseX * 15}px,
-//             ${mouseY * 15}px
-//         )`;
-
-
-//     /* FRONT */
-
-//     front.style.transform =
-//         `translate(
-//             ${mouseX * 25}px,
-//             ${mouseY * 25}px
-//         )`;
-
-
-//     /* IMAGE */
-
-//     image.style.transform =
-//         `translate(
-//             ${mouseX * 40}px,
-//             ${mouseY * 40}px
-//         )`;
-
-
-//     /* =========================
-//        CHANGE TEXT
-//        ========================= */
-
-//     if (mouseX < -0.5) {
-//     changeProject(0);
-// }
-// else if (mouseX < 0) {
-//     changeProject(1);
-// }
-// else if (mouseX < 0.5) {
-//     changeProject(2);
-// }
-// else {
-//     changeProject(3);
-// }
-
-// });
-
-
-// /* =========================
-//    RESET WHEN MOUSE LEAVES
-//    ========================= */
-
-// card.addEventListener("mouseleave", function() {
-
-//     back.style.transform =
-//         "translate(0, 0)";
-
-//     middle.style.transform =
-//         "translate(0, 0)";
-
-//     front.style.transform =
-//         "translate(0, 0)";
-
-//     image.style.transform =
-//         "translate(0, 0)";
-
-// });
-
-// // const card = document.querySelector(".project-card");
-// // const front = document.querySelector(".front");
-
-// // card.addEventListener("mousemove", function(event) {
-
-// //     const rect = card.getBoundingClientRect();
-
-// //     const mouseX =
-// //         (event.clientX - rect.left) / rect.width * 2 - 1;
-
-// //     const mouseY =
-// //         (event.clientY - rect.top) / rect.height * 2 - 1;
-
-// //     front.style.transform =
-// //         `translate(${mouseX * 40}px, ${mouseY * 40}px)`;
-// // }
+    // Start from Japanese next time
+    currentIndex = 0;
+});
